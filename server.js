a77a9cf0a51df72fdcf03493f93c4645551e4539
Tobi Balogun <tobi.balo@gmail.com>
2022-08-30 09:02:26 +0100

    Add ejs

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "The first of it",
+      createdAt: new Date(),
+      description: "We have described!",
+    },
+    {
+      title: "The second of it",
+      createdAt: new Date(),
+      description: "We have described, again!",
+    },
+  ]
+  res.render("index", { articles: articles })
+})
+
 const PORT = 8020
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is listening on ${PORT}`)
