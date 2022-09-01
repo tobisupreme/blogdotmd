8dd46966d8697e08e55510d42614158675d41071
Tobi Balogun <tobi.balo@gmail.com>
2022-09-01 06:54:07 +0100

    Define and initialise articles schema

--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
   res.render("articles/new")
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown
+  })
+  try {
+    article = await article.save()
+    res.redirect(`/articles/`)
+  } catch (e) {
+    console.log(e)
+  }
 
 })
 
