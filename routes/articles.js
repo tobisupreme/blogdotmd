134f0c57b7c6fdfe12e4c3c80bfafb9cd16cbe7e
Tobi Balogun <tobi.balo@gmail.com>
2022-09-01 12:55:00 +0100

    Implement Delete articles

--- routes/articles.js
+++ routes/articles.js
@@ -28,4 +28,10 @@ router.post('/', async (req, res) => {
 
 })
 
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id)
+
+  res.redirect("/")
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const Article = require("./models/article")
 const articleRouter = require("./routes/articles")
+const methodOverride = require("method-override")
 const app = express()
 
 mongoose.connect('mongodb://localhost/markdownblog')
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/markdownblog')
 app.set("view engine", "ejs")
 
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride("_method"))
 app.use("/articles/", articleRouter)
 
 app.get("/", async (req, res) => {
